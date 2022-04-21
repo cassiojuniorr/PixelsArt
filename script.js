498a78f9b2cbb28597aa7f687e726f31c6d8ab73
cassio <cassiospcjunior@gmail.com>
2022-04-21 00:59:06 -0300

    1 ao 6

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function createBoard(){
+    let contB = document.getElementById("pixel-board");
+    for (let cont = 0; cont < 5; cont++){
+        let sec = document.createElement("section");
+        for (let index = 0; index < 5; index++){
+            let divs = document.createElement("div");
+            divs.classList.add("pixel");
+            sec.appendChild(divs);
+        }
+        contB.appendChild(sec);
+    }
+}
+createBoard();
+
